e67a000fa982f86e67325eb81c62f0bec1cb5427
Gantulga0722 <mgtulga22@gmail.com>
2024-02-06 23:38:55 +0800

    From Home

---
+++ src/pages/api/article/id.js
@@ -0,0 +1,7 @@
+export default async function handler(request, response) {
+  const id = request.query.id;
+  const data = await fetch(`https://dev.to/api/articles?id=${id}`);
+  const blog = await data.json();
+  response.setHeader("Access-Control-Allow-Origin", "*");
+  response.status(200).json(blog);
+}
