47f805b5cb947f83e4a7648a038a4110efdab1cf
Gantulga0722 <mgtulga22@gmail.com>
2024-02-09 23:12:35 +0800

    from Home

--- src/pages/api/lmPost.js
+++ src/pages/api/lmPost.js
@@ -2,7 +2,7 @@ export default async function handler(request, response) {
   const page = request.query.page;
   const perPage = request.query.per_page;
   const data = await fetch(
-    `https://dev.to/api/articles?tag=branding&per_page=3&page=${page}`
+    `https://dev.to/api/articles?per_page=3&page=${page}`
   );
   const blog = await data.json();
   response.setHeader("Access-Control-Allow-Origin", "*");
